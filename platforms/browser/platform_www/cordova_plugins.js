170a1fb64894057b9c54d77c27b1aa68b5aa04d0
Tom George <utduu@student.kit.edu>
2018-01-14 22:32:22 +0100

    bluetooth functionality added

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -7,12 +7,30 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.screenorientation"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ble-central/www/ble.js",
+        "id": "cordova-plugin-ble-central.ble",
+        "pluginId": "cordova-plugin-ble-central",
+        "clobbers": [
+            "ble"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ble-central/src/browser/BLECentralPlugin.js",
+        "id": "cordova-plugin-ble-central.BLECentralPlugin",
+        "pluginId": "cordova-plugin-ble-central",
+        "merges": [
+            "ble"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-screen-orientation": "3.0.1"
+    "cordova-plugin-screen-orientation": "3.0.1",
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-ble-central": "1.1.4"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,10 +1,40 @@
 var LINEWIDTH_COLUMN_LINE = 10;
 
+document.addEventListener("deviceready", initialize, false);
+
+function scan_and_connect_glove(){
+    document.getElementById("msg").innerHTML = "Scanning";
+    ble.scan([], 5, function(device){
+	if(device.name == "TECO Wearable 4"){
+	    ble.connect(device.id, connect_success, connect_fail);
+	}
+    }, function(){
+	document.getElementById("msg").innerHTML = "Scan fail";
+    });
+}
+
+function ble_disable(){
+    document.getElementById("msg").innerHTML = "You have to enable bluetooth."
+}
+
+function connect_success(peripheral){
+    document.getElementById("msg").innerHTML = "Erfolgreich connected";
+}
+
+function connect_fail(peripheral){
+    document.getElementById("msg").innerHTML = "Fehler beim connecten";
+}
+
 function initialize(){
-    //document.addEventListener('deviceready', onDeviceReady);
+    document.getElementById("msg").innerHTML = ble;
+    ble.isEnabled(scan_and_connect_glove, ble_disabled);
+    
     var btn = document.getElementById("go_btn");
     btn.addEventListener('touchstart', go_btn_touched);
     btn.addEventListener('mousedown', go_btn_touched);
+    
+    btn.addEventListener('touchend', go_btn_released);
+    btn.addEventListener('mouseup', go_btn_released);
     
     document.documentElement.style.webkitTouchCallout = "none";
     document.documentElement.style.webkitUserSelect = "none";
@@ -17,106 +47,29 @@ function initialize(){
 
 function go_btn_touched(event){
     event.target.setAttribute("src", "./img/go_btn_pressed.png");
+}
+
+function go_btn_released(event){
     window.open("./game_surface.html", "_self", false);
 }
 
 
 function startGame(){
-     // create game surface
-    var c = document.createElement("canvas");
-    c.width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    c.height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-    c.setAttribute("id", "game_canvas");
-    document.body.appendChild(c)
-
-    drawGameSurfaceDigit(3);
-    setTimeout(function(){
-	clearGameSurface();
-	drawGameSurfaceDigit(2);
-    }, 1000);
-    setTimeout(function(){
-	clearGameSurface();
-	drawGameSurfaceDigit(1);
-	}, 2000);
-    setTimeout(function(){
-	clearGameSurface();
-	drawGameSurface();
-    }, 3000);
-    setTimeout(function(){
-	c.addEventListener("touchstart", surface_pressed);
-	c.addEventListener("mousedown", surface_pressed);
-	c.addEventListener("mouseup", surface_released);
-	c.addEventListener("touchend", surface_released);
-    }, 3000);
-}
-
-function drawGameSurfaceDigit(digit){
-    var ctx = document.getElementById("game_canvas").getContext('2d');
-    var width = ctx.canvas.width/4;
+    // initialise bluetooth stuff
     
-    ctx.lineWidth = LINEWIDTH_COLUMN_LINE;
-    // draw 4 vertical lines
-    for(var i = 1; i<4; ++i){
-    	ctx.beginPath();
-    	ctx.moveTo(i*width,0);
-    	ctx.lineTo(i*width,ctx.canvas.height);
-    	ctx.stroke();
-    }
+    // start countdown
 
-    ctx.fillStyle = "#FF0000";
-    ctx.font = "150px Arial";
-    ctx.textAlign = "center";
-    ctx.fillText(digit,ctx.canvas.width/2,ctx.canvas.height/2+50);
-}
-
-function drawGameSurface(){
-    var ctx = document.getElementById("game_canvas").getContext('2d');
-    var width = ctx.canvas.width/4;
-    console.log(width);
+    // start 
     
-    ctx.lineWidth = 10;
-    // draw 4 vertical lines
-    for(var i = 1; i<4; ++i){
-	ctx.beginPath();
-	ctx.moveTo(i*width,0);
-	ctx.lineTo(i*width,ctx.canvas.height);
-	ctx.stroke();
-    }
 }
 
-function clearGameSurface(){
-    var ctx = document.getElementById("game_canvas").getContext('2d');
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+
+function div_pressed(event){
+    event.target.style.backgroundColor = "#000000";
 }
 
-function surface_pressed(event){
-    var x = event.layerX;
-    var column_width = event.target.width/4;
-    
-    var column = Math.floor(x/column_width);
-    color_column(column, "#228B22");
-}
+function div_released(event){
+    event.target.style.backgroundColor = "#FFFFFF";
 
-function surface_released(event){
-    var x = event.layerX;
-    var column_width = event.target.width/4;
-    
-    var column = Math.floor(x/column_width);
-    color_column(column, "#FFFFFF");
 }
-
-function color_column(no,color){
-    var column_width = document.getElementById("game_canvas").width/4;
-
-    var ctx = document.getElementById("game_canvas").getContext("2d");
-    ctx.fillStyle = color;
-    if(no==0){
-	ctx.fillRect(0,0,column_width-LINEWIDTH_COLUMN_LINE/2,ctx.canvas.height);
-    }
-    else if(no==1 || no==2){
-	ctx.fillRect(no*column_width+LINEWIDTH_COLUMN_LINE/2,0,column_width-LINEWIDTH_COLUMN_LINE,ctx.canvas.height);
-    }
-    else{
-	ctx.fillRect(3*column_width+LINEWIDTH_COLUMN_LINE/2,0,column_width-LINEWIDTH_COLUMN_LINE/2,ctx.canvas.height);
-    }
-}
+			   
